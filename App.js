f8f31523711f82127000c43fad0e1cf623b2006f
Kevin Lee <leekevin@berkeley.edu>
2018-11-03 10:28:21 -0700

    Add directions page

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+import HomeScreen from './HomeScreen';
+import Directions from './Directions';
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Directions: Directions,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    return <RootStack />;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
