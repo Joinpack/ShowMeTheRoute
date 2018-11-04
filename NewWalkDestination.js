41e0c4dd469a5516ebce6afdcafb1cb6a4616459
Nimalen Sivapalan <nsivapalan@berkeley.edu>
2018-11-03 23:56:04 -0700

    add dropdown menus for Location and Destination

--- NewWalkDestination.js
+++ NewWalkDestination.js
@@ -3,7 +3,7 @@ NewWalkDestination solicits user input destination data in order to create a NEW
 */
 
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Picker, PickerIOS } from 'react-native';
 
 
 class NewWalkScreenDestination extends React.Component {
@@ -21,10 +21,26 @@ class NewWalkScreenDestination extends React.Component {
        <View style = {styles.container}>
          <TextInput
            style={{height: 500, width: 1000, textAlign: 'center', fontSize: 30}}
-           placeholder="What is your destination?"
+           placeholder="What is your closest destination?"
            onChangeText={(destination) => this.setState({destination})}
            value={this.state.destination}
          />
+         <View style={{width: 300, height: 500, marginBottom: 10}}>
+           <Picker selectedValue={this.state.destination}
+                      onValueChange={(destination) => this.setState({destination})}>
+              <Picker.Item key="Dorms: Unit 1" label="Dorms: Unit 1" value="Dorms: Unit 1" />
+              <Picker.Item key="Dorms: Unit 2" label="Dorms: Unit 2" value="Dorms: Unit 2" />
+              <Picker.Item key="Dorms: Unit 3/Blackwell Hall" label="Dorms: Unit 3/Blackwell Hall" value="Dorms: Unit 3/Blackwell Hall" />
+              <Picker.Item key="Dorms: Foothill" label="Dorms: Foothill" value="Dorms: Foothill" />
+              <Picker.Item key="Dorms: Bowles Hall" label="Dorms: Bowles Hall" value="Dorms: Bowles Hall" />
+              <Picker.Item key="Dorms: Channing-Bowditch" label="Dorms: Channing-Bowditch" value="Dorms: Channing-Bowditch" />
+              <Picker.Item key="Downtown Berkeley BART" label="Downtown Berkeley BART" value="Downtown Berkeley BART" />
+              <Picker.Item key="Northside" label="Northside" value="Northside" />
+              <Picker.Item key="Frat Row" label="Frat Row" value="Frat Row" />
+              <Picker.Item key="Southside/Other" label="Southside/Other" value="Southside/Other" />
+
+            </Picker>
+          </View>
          <View style = {styles.buttonContainer}>
            <Button
              title="Next"
@@ -43,6 +59,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'powderblue',
     alignItems: 'center',
+    justifyContent: 'space-around',
   },
   buttonContainer: {
     marginBottom: 100,
--- NewWalkLocation.js
+++ NewWalkLocation.js
@@ -3,7 +3,7 @@ NewWalkLocation solicits user input location data in order to create a NEW WALK.
 */
 
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Picker, PickerIOS } from 'react-native';
 
 
 class NewWalkScreenLocation extends React.Component {
@@ -20,11 +20,27 @@ class NewWalkScreenLocation extends React.Component {
      return (
        <View style = {styles.container}>
          <TextInput
-           placeholder="Where are you?"
+           placeholder="Where are you closest to?"
            style={{height: 500, width: 1000, textAlign: 'center', fontSize: 30}}
            onChangeText={(location) => this.setState({location})}
            value={this.state.location}
          />
+         <View style={{width: 300, height: 500, marginBottom: 10}}>
+           <Picker selectedValue={this.state.location}
+                      onValueChange={(location) => this.setState({location})}>
+              <Picker.Item key="Dwinelle Hall" label="Dwinelle Hall" value="Dwinelle Hall" />
+              <Picker.Item key="VLSB" label="VLSB" value="VLSB" />
+              <Picker.Item key="Li Ka Shing Center" label="Li Ka Shing Center" value="Li Ka Shing Center" />
+              <Picker.Item key="Soda Hall" label="Soda Hall" value="Soda Hall" />
+              <Picker.Item key="Doe Library" label="Doe Library" value="Doe Library" />
+              <Picker.Item key="Moffitt Library" label="Moffitt Library" value="Moffitt Library" />
+              <Picker.Item key="Engineering Library" label="Engineering Library" value="Engineering Library" />
+              <Picker.Item key="Business Library" label="Business Library" value="Business Library" />
+              <Picker.Item key="Chemistry Library" label="Chemistry Library" value="Chemistry Library" />
+              <Picker.Item key="California Memorial Stadium" label="California Memorial Stadium" value="California Memorial Stadium" />
+
+            </Picker>
+          </View>
          <View style = {styles.buttonContainer}>
            <Button
              title="Next"
