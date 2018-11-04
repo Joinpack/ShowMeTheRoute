b9c7dd5ed9f11dd7a242ee71c5af30d43ded64b9
Kevin Lee <leekevin@berkeley.edu>
2018-11-03 23:08:29 -0700

    Remove unnecessary code in home

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-import logIn from './Login';
 
 class HomeScreen extends React.Component {
   static navigationOptions = {
